243873fb9e2222a096c642fe8c61affcd70420f8
jemmyasjd <jemmy33.jd@gmail.com>
2024-04-15 23:45:01 +0530

    added vercel.json

--- server/index.js
+++ server/index.js
@@ -31,6 +31,7 @@ const server = app.listen(process.env.PORT, () =>
 const io = socket(server, {
   cors: {
     origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
     credentials: true,
   },
 });
